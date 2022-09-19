1bad34a07561707fe44c10c76b30e6f6b42277d8
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-09-19 15:27:47 -0300

    chore: modify language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    i18n: {
-      defaultLocale: 'en',
-      locales: ['en', 'fr', 'de'],
-    },
-  };
+  i18n: {
+    defaultLocale: "pt",
+    locales: ["pt", "en"]
+  }
+};
